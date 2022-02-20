5668c6c63cafe6071854daeffe03af4177f41caa
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2022-02-20 11:38:27 +0330

    Added login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,7 +42,20 @@ const registerUser = asyncHandler(async function (req, res) {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async function (req, res) {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 // @desc    Get user data
